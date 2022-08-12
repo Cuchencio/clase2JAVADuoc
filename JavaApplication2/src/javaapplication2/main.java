bbf22e3ce2d800ec0956ef3242bc43ddd1adc95b
Cucho <ces.maureira@duocuc.cl>
2022-08-11 20:19:01 -0400

    Cambio 2

--- JavaApplication2/src/javaapplication2/main.java
+++ JavaApplication2/src/javaapplication2/main.java
@@ -15,6 +15,8 @@ public class main {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        String hola = "Berga";
+        System.out.println(hola);
     }
     
 }
